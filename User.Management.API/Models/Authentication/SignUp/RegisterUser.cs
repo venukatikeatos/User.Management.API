b4c2cda6a9930274bb8363f6861f81a71ed3e4d7
venukatikeatos <venu.katike@atos.net>
2024-06-19 12:50:16 +0530

    added some models

---
+++ User.Management.API/Models/Authentication/SignUp/RegisterUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.Management.API.Models.Authentication.SignUp
+{
+    public class RegisterUser
+    {
+        [Required(ErrorMessage ="User Name is required")]
+        public string ? UserName { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        public string? Password { get; set; }
+    }
+}
--- User.Management.API/ReadMe/ReadMe.txt
+++ User.Management.API/ReadMe/ReadMe.txt
@@ -3,3 +3,6 @@ Very First Step To add below pakages :
 	2.Microsoft.EntityFrameworkCore.Tools
 	3.MS.AspNetCore.Identity.EntityFrameworkCore
 	4.MS.AspNetCore.Authentication.JwtBearer
+
+	Code with salman YT
+	https://www.youtube.com/watch?v=WpymlVGek94&list=PLX4n-znUpc2b19AoYa4BMuhGuRnZItJK_
